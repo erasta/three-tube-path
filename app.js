33ee1850eb0eb2e18c44c3984a8431115f6f974e
Eran Geva <eran.geva@gmail.com>
2022-09-02 22:34:51 +0300

    lil gui

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
+import { RoomEnvironment } from './node_modules/three/examples/jsm/environments/RoomEnvironment.js';
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
 import { TubePath } from './TubePath.js';
+import { GUI } from './node_modules/three/examples/jsm/libs/lil-gui.module.min.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -10,18 +12,37 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
+scene.environment = new THREE.PMREMGenerator(renderer).fromScene(new RoomEnvironment()).texture;
+scene.background = new THREE.Color(0x888888);
 
-const path = new THREE.CatmullRomCurve3([
-    new THREE.Vector3(-10, 0, 10),
-    new THREE.Vector3(-5, 5, 5),
-    new THREE.Vector3(0, 0, 0),
-    new THREE.Vector3(5, -5, 5),
-    new THREE.Vector3(10, 0, 10)
-], false, 'chordal');
-const geometry = new TubePath(path, undefined, 1, 8, false);
-const tube = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 'green' }));
-tube.add(new THREE.LineSegments(new THREE.WireframeGeometry(tube.geometry)));
+const gui = new GUI();
+
+const params = {
+    radialSegments: 8,
+};
+
+gui.add(params, 'radialSegments').min(3).max(24).step(1).onChange(recreate);
+
+const tube = new THREE.Mesh(new THREE.BufferGeometry(), new THREE.MeshStandardMaterial({ color: 'green' }));
 scene.add(tube);
+
+function recreate() {
+    const path = new THREE.CatmullRomCurve3([
+        new THREE.Vector3(-10, 0, 10),
+        new THREE.Vector3(-5, 5, 5),
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(5, -5, 5),
+        new THREE.Vector3(10, 0, 10)
+    ], false, 'chordal');
+    tube.geometry = new TubePath(path, undefined, 1, params.radialSegments, false);
+    if (!tube.wire) {
+        tube.wire = new THREE.LineSegments(new THREE.BufferGeometry(), new THREE.LineBasicMaterial({ color: 'cyan' }));
+        tube.add(tube.wire);
+    }
+    tube.wire.geometry = new THREE.WireframeGeometry(tube.geometry);
+}
+
+recreate();
 
 function animate() {
     requestAnimationFrame(animate);
