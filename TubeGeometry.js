e90bc72ca29e0cf71d98e49f1d7661f6c1a140a0
Eran Geva <eran.geva@gmail.com>
2022-09-02 11:45:10 +0300

    tube path

---
+++ TubeGeometry.js
@@ -0,0 +1,317 @@
+import {
+	BufferGeometry,
+	Float32BufferAttribute,
+	MathUtils,
+	Matrix4,
+	Vector2,
+	Vector3
+} from 'three';
+import * as Curves from '../node_modules/three/src/extras/curves/Curves.js';
+
+class TubeGeometry extends BufferGeometry {
+
+	constructor(path = new Curves['QuadraticBezierCurve3'](new Vector3(- 1, - 1, 0), new Vector3(- 1, 1, 0), new Vector3(1, 1, 0)),
+		uMappingFrames = Array(64).fill().map((x, i, arr) => i / (arr.length - 1)),
+		radius = 1,
+		radialSegments = 8,
+		closed = false) {
+
+		super();
+
+		this.type = 'TubeGeometry';
+
+		this.parameters = {
+			path: path,
+			uMappingFrames: uMappingFrames,
+			radius: radius,
+			radialSegments: radialSegments,
+			closed: closed
+		};
+
+		const frames = this.computeFrenetFrames( path, uMappingFrames, closed );
+
+		// expose internals
+
+		this.tangents = frames.tangents;
+		this.normals = frames.normals;
+		this.binormals = frames.binormals;
+
+		// helper variables
+
+		const vertex = new Vector3();
+		const normal = new Vector3();
+		const uv = new Vector2();
+		let P = new Vector3();
+
+		// buffer
+
+		const vertices = [];
+		const normals = [];
+		const uvs = [];
+		const indices = [];
+
+		// create buffer data
+
+		generateBufferData();
+
+		// build geometry
+
+		this.setIndex( indices );
+		this.setAttribute( 'position', new Float32BufferAttribute( vertices, 3 ) );
+		this.setAttribute( 'normal', new Float32BufferAttribute( normals, 3 ) );
+		this.setAttribute( 'uv', new Float32BufferAttribute( uvs, 2 ) );
+
+		// functions
+
+		function generateBufferData() {
+
+			for ( let i = 0; i < uMappingFrames.length - 1; i ++ ) {
+
+				generateSegment( i );
+
+			}
+
+			// if the geometry is not closed, generate the last row of vertices and normals
+			// at the regular position on the given path
+			//
+			// if the geometry is closed, duplicate the first row of vertices and normals (uvs will differ)
+
+			generateSegment( ( closed === false ) ? uMappingFrames.length - 1 : 0 );
+
+			// uvs are generated in a separate function.
+			// this makes it easy compute correct values for closed geometries
+
+			generateUVs();
+
+			// finally create faces
+
+			generateIndices();
+
+		}
+
+		function generateSegment( i ) {
+
+			// we use getPointAt to sample evenly distributed points from the given path
+
+			P = path.getPointAt( uMappingFrames[ i ], P );
+
+			// retrieve corresponding normal and binormal
+
+			const N = frames.normals[ i ];
+			const B = frames.binormals[ i ];
+
+			// generate normals and vertices for the current segment
+
+			for ( let j = 0; j <= radialSegments; j ++ ) {
+
+				const v = j / radialSegments * Math.PI * 2;
+
+				const sin = Math.sin( v );
+				const cos = - Math.cos( v );
+
+				// normal
+
+				normal.x = ( cos * N.x + sin * B.x );
+				normal.y = ( cos * N.y + sin * B.y );
+				normal.z = ( cos * N.z + sin * B.z );
+				normal.normalize();
+
+				normals.push( normal.x, normal.y, normal.z );
+
+				// vertex
+
+				vertex.x = P.x + radius * normal.x;
+				vertex.y = P.y + radius * normal.y;
+				vertex.z = P.z + radius * normal.z;
+
+				vertices.push( vertex.x, vertex.y, vertex.z );
+
+			}
+
+		}
+
+		function generateIndices() {
+
+			for ( let j = 1; j <= uMappingFrames.length - 1; j ++ ) {
+
+				for ( let i = 1; i <= radialSegments; i ++ ) {
+
+					const a = ( radialSegments + 1 ) * ( j - 1 ) + ( i - 1 );
+					const b = ( radialSegments + 1 ) * j + ( i - 1 );
+					const c = ( radialSegments + 1 ) * j + i;
+					const d = ( radialSegments + 1 ) * ( j - 1 ) + i;
+
+					// faces
+
+					indices.push( a, b, d );
+					indices.push( b, c, d );
+
+				}
+
+			}
+
+		}
+
+		function generateUVs() {
+
+			for ( let i = 0; i <= uMappingFrames.length - 1; i ++ ) {
+
+				for ( let j = 0; j <= radialSegments; j ++ ) {
+
+					uv.x = i / (uMappingFrames.length - 1);
+					uv.y = j / radialSegments;
+
+					uvs.push( uv.x, uv.y );
+
+				}
+
+			}
+
+		}
+
+	}
+
+	computeFrenetFrames( path, uMappingFrames, closed ) {
+
+		// see http://www.cs.indiana.edu/pub/techreports/TR425.pdf
+
+		const normal = new Vector3();
+
+		const tangents = [];
+		const normals = [];
+		const binormals = [];
+
+		const vec = new Vector3();
+		const mat = new Matrix4();
+
+		const segments = uMappingFrames.length - 1;
+
+		// compute the tangent vectors for each segment on the curve
+
+		for ( let i = 0; i <= segments; i ++ ) {
+
+			const u = uMappingFrames[ i ];
+
+			tangents[ i ] = path.getTangentAt( u, new Vector3() );
+
+		}
+
+		// select an initial normal vector perpendicular to the first tangent vector,
+		// and in the direction of the minimum tangent xyz component
+
+		normals[ 0 ] = new Vector3();
+		binormals[ 0 ] = new Vector3();
+		let min = Number.MAX_VALUE;
+		const tx = Math.abs( tangents[ 0 ].x );
+		const ty = Math.abs( tangents[ 0 ].y );
+		const tz = Math.abs( tangents[ 0 ].z );
+
+		if ( tx <= min ) {
+
+			min = tx;
+			normal.set( 1, 0, 0 );
+
+		}
+
+		if ( ty <= min ) {
+
+			min = ty;
+			normal.set( 0, 1, 0 );
+
+		}
+
+		if ( tz <= min ) {
+
+			normal.set( 0, 0, 1 );
+
+		}
+
+		vec.crossVectors( tangents[ 0 ], normal ).normalize();
+
+		normals[ 0 ].crossVectors( tangents[ 0 ], vec );
+		binormals[ 0 ].crossVectors( tangents[ 0 ], normals[ 0 ] );
+
+
+		// compute the slowly-varying normal and binormal vectors for each segment on the curve
+
+		for ( let i = 1; i <= segments; i ++ ) {
+
+			normals[ i ] = normals[ i - 1 ].clone();
+
+			binormals[ i ] = binormals[ i - 1 ].clone();
+
+			vec.crossVectors( tangents[ i - 1 ], tangents[ i ] );
+
+			if ( vec.length() > Number.EPSILON ) {
+
+				vec.normalize();
+
+				const theta = Math.acos( MathUtils.clamp( tangents[ i - 1 ].dot( tangents[ i ] ), - 1, 1 ) ); // clamp for floating pt errors
+
+				normals[ i ].applyMatrix4( mat.makeRotationAxis( vec, theta ) );
+
+			}
+
+			binormals[ i ].crossVectors( tangents[ i ], normals[ i ] );
+
+		}
+
+		// if the curve is closed, postprocess the vectors so the first and last normal vectors are the same
+
+		if ( closed === true ) {
+
+			let theta = Math.acos( MathUtils.clamp( normals[ 0 ].dot( normals[ segments ] ), - 1, 1 ) );
+			theta /= segments;
+
+			if ( tangents[ 0 ].dot( vec.crossVectors( normals[ 0 ], normals[ segments ] ) ) > 0 ) {
+
+				theta = - theta;
+
+			}
+
+			for ( let i = 1; i <= segments; i ++ ) {
+
+				// twist a little...
+				normals[ i ].applyMatrix4( mat.makeRotationAxis( tangents[ i ], theta * i ) );
+				binormals[ i ].crossVectors( tangents[ i ], normals[ i ] );
+
+			}
+
+		}
+
+		return {
+			tangents: tangents,
+			normals: normals,
+			binormals: binormals
+		};
+
+	}
+
+	toJSON() {
+
+		const data = super.toJSON();
+
+		data.path = this.parameters.path.toJSON();
+
+		return data;
+
+	}
+
+	static fromJSON( data ) {
+
+		// This only works for built-in curves (e.g. CatmullRomCurve3).
+		// User defined curves or instances of CurvePath will not be deserialized.
+		return new TubeGeometry(
+			new Curves[ data.path.type ]().fromJSON( data.path ),
+			data.tubularSegments,
+			data.radius,
+			data.radialSegments,
+			data.closed
+		);
+
+	}
+
+}
+
+
+export { TubeGeometry };
---
+++ app.js
@@ -0,0 +1,76 @@
+import * as THREE from 'three';
+import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { TubeGeometry } from './TubeGeometry.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(20, 20, 20);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// const path = new THREE.SplineCurve(new THREE.Vector3(- 1, - 1, 0), new THREE.Vector3(- 1, 1, 0), new THREE.Vector3(1, 1, 0));
+const path = new THREE.CatmullRomCurve3([
+    new THREE.Vector3(-10, 0, 10),
+    new THREE.Vector3(-5, 5, 5),
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(5, -5, 5),
+    new THREE.Vector3(10, 0, 10)
+], false, 'chordal');
+const lengths = [0];
+path.points.forEach((p, i, arr) => {
+    if (i > 0) {
+        const last = lengths.at(-1);
+        const next = last + p.distanceTo(arr[i - 1]);
+        if (i > 1) {
+            lengths.push(last + 0.1);
+            lengths.push(last + 0.3);
+            lengths.push(last + 0.5);
+        }
+        if (i < arr.length - 1) {
+            lengths.push(next - 0.5);
+            lengths.push(next - 0.3);
+            lengths.push(next - 0.1);
+        }
+        lengths.push(next);
+    }
+});
+
+// const lengths1 = [0];
+// lengths.forEach((l, i, a) => {
+//     if (i > 0) {
+//         lengths1.push(lengths1.at(-1) + 0.5);
+//         lengths1.push(l - 0.5);
+//         lengths1.push(l);
+//     }
+// })
+console.log(lengths);
+const uMappingFrames = lengths.map(l => l / lengths.at(-1));
+console.log(uMappingFrames);
+// const geometry1 = new THREE.TubeGeometry(path, 1, 1, 3, true);
+const geometry = new TubeGeometry(path, uMappingFrames, 1, 8, false);
+
+// console.log( 'org', geometry1.index.array);
+// console.log( 'new', geometry.index.array);
+// console.log( geometry.index.array.findIndex((v, i) => v !== geometry1.index.array[i]));
+// function comp(attr) {
+//     console.log(attr, 'org', geometry1.attributes[attr].array);
+//     console.log(attr, 'new', geometry.attributes[attr].array);
+//     console.log(attr, geometry.attributes[attr].array.findIndex((v, i) => v !== geometry1.attributes[attr].array[i]));
+// }
+// comp('position');
+// comp('normal');
+// comp('uv');
+// geometry.computeBoundingSphere();
+const cube = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 'green' }));
+scene.add(cube);
+cube.add(new THREE.LineSegments(new THREE.WireframeGeometry(cube.geometry)));
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+};
+
+animate();
